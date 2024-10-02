796f66d40dad5d7bb303817ea6017c7658ad3ff9
Arshad Mahemood Patel <arshad.amp432@gmail.com>
2024-10-02 21:48:20 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript to handle future interactions
+
+// Placeholder for login function
+function login() {
+    alert('Login functionality coming soon!');
+}
+
+// Placeholder for sign-up function
+function signup() {
+    alert('Sign-up functionality coming soon!');
+}
