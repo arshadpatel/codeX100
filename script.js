7adf670d22b1f54ed39c1ec8aec701c30499075c
himanshi <narendraidni261122@gmail.com>
2024-10-03 23:08:20 +0530

    functionailty-to-hamburger

--- script.js
+++ script.js
@@ -15,4 +15,14 @@ function signup() {
 const hamburger = document.querySelector('.hide');
 const navLinks = document.querySelector('.nav-links');
 
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show-nav-links');
+  if (navLinks.classList.contains('show-nav-links')) {
+    navLinks.classList.remove('hide1');
+    hamburger.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  } else {
+    navLinks.classList.add('hide1');
+    hamburger.innerHTML = '<i class="fa-solid fa-bars"></i>';
+  }
+});
 
