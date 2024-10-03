bfa4b39facd763d66bb3fb5d6420cd5f60b112d9
himanshi <narendraidni261122@gmail.com>
2024-10-03 16:56:09 +0530

    add hamburger

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ function login() {
 function signup() {
     alert('Sign-up functionality coming soon!');
 }
+
+// add functionality to  hamburger 
+// Select the hamburger icon and navigation links
+const hamburger = document.querySelector('.hide');
+const navLinks = document.querySelector('.nav-links');
+
+
